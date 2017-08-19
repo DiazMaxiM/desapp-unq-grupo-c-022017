ae4d5e2c5bf44572ab1d362f037bd2aab4192882
rozaracho <zaracho.rosali@gmail.com>
2017-08-19 10:42:52 -0300

    Refactorizacion de nombres de la clase user

--- grupoc/src/main/java/model/User.java
+++ grupoc/src/main/java/model/User.java
@@ -1,10 +1,52 @@
 package model;
 
 public class User {
-	private String email;
-	private String userName;
-	private String userSurname;
+	
+	private String userfirstName;
+	private String userLastName;
+	private String userEmailAddress;
 	private Telephone userTelephone;
 	private Address userAddress;
+
+	public String getUserfirstName() {
+		return userfirstName;
+	}
+	
+	public void setUserfirstName(String userfirstName) {
+		this.userfirstName = userfirstName;
+	}
+
+	public String getUserLastName() {
+		return userLastName;
+	}
+
+	public void setUserLastName(String userLastName) {
+		this.userLastName = userLastName;
+	}
+
+	public String getUserEmailAddress() {
+		return userEmailAddress;
+	}
+
+	public void setUserEmailAddress(String userEmailAddress) {
+		this.userEmailAddress = userEmailAddress;
+	}
+
+	public Telephone getUserTelephone() {
+		return userTelephone;
+	}
+
+	public void setUserTelephone(Telephone userTelephone) {
+		this.userTelephone = userTelephone;
+	}
+
+	public Address getUserAddress() {
+		return userAddress;
+	}
+
+	public void setUserAddress(Address userAddress) {
+		this.userAddress = userAddress;
+	}
+	
 
 }
