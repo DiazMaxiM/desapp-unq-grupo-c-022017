aa3961c289db2aa2248b005ab0868212a3af46dc
rozaracho <zaracho.rosali@gmail.com>
2017-08-19 18:11:06 -0300

    Se agrego la informacion del menu

--- grupoc/src/main/java/model/Menu.java
+++ grupoc/src/main/java/model/Menu.java
@@ -1,5 +1,111 @@
 package model;
 
-public class Menu {
+import java.util.Date;
+import java.util.List;
 
+public class Menu {
+    private String menuName;
+    private String menuDescription;
+    private Category menuCategory;
+    private Float menuDeliveryPrice;
+    private Date startDateOfferMenu;
+    private Date endDateOfferMenu;
+    private List<Double> deliveryTimesMenus;
+    private Float averageDeliveryTimeOfMenu;
+    private Float menuPrice;
+    private Integer firstMinimumNumberOfMenusToBuy;
+    private Float firstminimumPriceOfMenusToBuy;
+    private Integer secondMinimumNumberOfMenusToBuy;
+    private Float secondMinimumPriceOfMenusToBuy;
+    private Float maximumNumberOfMunusSalesPerDay;
+    
+	public String getMenuName() {
+		return menuName;
+	}
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+	public String getMenuDescription() {
+		return menuDescription;
+	}
+	public void setMenuDescription(String menuDescription) {
+		this.menuDescription = menuDescription;
+	}
+	public Category getMenuCategory() {
+		return menuCategory;
+	}
+	public void setMenuCategory(Category menuCategory) {
+		this.menuCategory = menuCategory;
+	}
+	public Float getMenuDeliveryPrice() {
+		return menuDeliveryPrice;
+	}
+	public void setMenuDeliveryPrice(Float menuDeliveryPrice) {
+		this.menuDeliveryPrice = menuDeliveryPrice;
+	}
+	public Date getStartDateOfferMenu() {
+		return startDateOfferMenu;
+	}
+	public void setStartDateOfferMenu(Date startDateOfferMenu) {
+		this.startDateOfferMenu = startDateOfferMenu;
+	}
+	public Date getEndDateOfferMenu() {
+		return endDateOfferMenu;
+	}
+	public void setEndDateOfferMenu(Date endDateOfferMenu) {
+		this.endDateOfferMenu = endDateOfferMenu;
+	}
+	public List<Double> getDeliveryTimesMenus() {
+		return deliveryTimesMenus;
+	}
+	public void setDeliveryTimesMenus(List<Double> deliveryTimesMenus) {
+		this.deliveryTimesMenus = deliveryTimesMenus;
+	}
+	public Float getAverageDeliveryTimeOfMenu() {
+		return averageDeliveryTimeOfMenu;
+	}
+	public void setAverageDeliveryTimeOfMenu(Float averageDeliveryTimeOfMenu) {
+		this.averageDeliveryTimeOfMenu = averageDeliveryTimeOfMenu;
+	}
+	public Float getMenuPrice() {
+		return menuPrice;
+	}
+	public void setMenuPrice(Float menuPrice) {
+		this.menuPrice = menuPrice;
+	}
+	public Integer getFirstMinimumNumberOfMenusToBuy() {
+		return firstMinimumNumberOfMenusToBuy;
+	}
+	public void setFirstMinimumNumberOfMenusToBuy(Integer firstMinimumNumberOfMenusToBuy) {
+		this.firstMinimumNumberOfMenusToBuy = firstMinimumNumberOfMenusToBuy;
+	}
+	public Float getFirstminimumPriceOfMenusToBuy() {
+		return firstminimumPriceOfMenusToBuy;
+	}
+	public void setFirstminimumPriceOfMenusToBuy(Float firstminimumPriceOfMenusToBuy) {
+		this.firstminimumPriceOfMenusToBuy = firstminimumPriceOfMenusToBuy;
+	}
+	public Integer getSecondMinimumNumberOfMenusToBuy() {
+		return secondMinimumNumberOfMenusToBuy;
+	}
+	public void setSecondMinimumNumberOfMenusToBuy(Integer secondMinimumNumberOfMenusToBuy) {
+		this.secondMinimumNumberOfMenusToBuy = secondMinimumNumberOfMenusToBuy;
+	}
+	public Float getSecondMinimumPriceOfMenusToBuy() {
+		return secondMinimumPriceOfMenusToBuy;
+	}
+	public void setSecondMinimumPriceOfMenusToBuy(Float secondMinimumPriceOfMenusToBuy) {
+		this.secondMinimumPriceOfMenusToBuy = secondMinimumPriceOfMenusToBuy;
+	}
+	public Float getMaximumNumberOfMunusSalesPerDay() {
+		return maximumNumberOfMunusSalesPerDay;
+	}
+	public void setMaximumNumberOfMunusSalesPerDay(Float maximumNumberOfMunusSalesPerDay) {
+		this.maximumNumberOfMunusSalesPerDay = maximumNumberOfMunusSalesPerDay;
+	}
+    
+    
+    
+    
+    
 }
