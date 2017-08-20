5691daef01b0f645030db2f22832d8fd9cfa56a8
Ingrid Calderón <ingridgcalderon@gmail.com>
2017-08-20 18:29:43 -0300

    La interface MenuSearchCriteria pasa a ser una clase abstracta

--- grupoc/src/main/java/menusearch/MenuSearchCriteria.java
+++ grupoc/src/main/java/menusearch/MenuSearchCriteria.java
@@ -1,11 +1,39 @@
 package menusearch;
 
-import java.util.List;
-
+import java.util.ArrayList;
 import model.Menu;
+import model.ServiceManager;
 
-public interface MenuSearchCriteria {
+public abstract class MenuSearchCriteria {
 	
-	public void menuSearch(List<Menu> menus);
-	
+	private ServiceManager serviceManager;
+	private Object criteria;
+				
+	public  ArrayList<Menu> menuSearch(){
+		ArrayList<Menu> menus = this.getServiceManager().getAllMenus();
+		ArrayList<Menu> result = new ArrayList<Menu>();
+		for(int i = 0; i < menus.size(); i++){
+			if(menus.get(i).getMenuCategory() == this.getCriteria()){
+				result.add(menus.get(i));
+			}
+		}
+		return result;
+	}
+
+	public Object getCriteria() {
+		return criteria;
+	}
+
+	public void setCriteria(Object criteria) {
+		this.criteria = criteria;
+	}
+
+	public ServiceManager getServiceManager() {
+		return serviceManager;
+	}
+
+	public void setServiceManager(ServiceManager serviceManager) {
+		this.serviceManager = serviceManager;
+	}
+
 }
