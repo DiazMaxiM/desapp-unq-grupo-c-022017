208ef624c006301b43709cca4f89bc285dc9a5a6
Ingrid Calderón <ingridgcalderon@gmail.com>
2017-08-20 15:40:44 -0300

    Agrego la clase de búsqueda de menús por categoría

---
+++ grupoc/src/main/java/menusearch/MenuSearchByCategory.java
@@ -0,0 +1,14 @@
+package menusearch;
+
+import java.util.List;
+
+import model.Menu;
+
+public class MenuSearchByCategory implements MenuSearchCriteria {
+
+	public void menuSearch(List<Menu> menus) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
