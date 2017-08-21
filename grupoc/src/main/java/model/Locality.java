c3311024372be72e434edd0128aa5c14d5aa3820
Ingrid Calderón <ingridgcalderon@gmail.com>
2017-08-21 14:04:53 -0300

    Agrego algunas localidades para pruebas

--- grupoc/src/main/java/model/Locality.java
+++ grupoc/src/main/java/model/Locality.java
@@ -1,5 +1,33 @@
 package model;
 
 public enum Locality {
-
+	ALMIRANTEBROWN("Almirante Brown"),
+	AVELLANEDA("Avellaneda"),
+	BERAZATEGUI("Berazategui"),
+	ENSENADA("Ensenada"),
+	ESCOBAR("Escobar"),
+	EZEIZA("Ezeiza"),
+	FLORENCIOVARELA("Florencio Varela"),
+	LAMATANZA("La Matanza"),
+	LAPLATA("La Plata"),
+	LANUS("Lanús"),
+	LOMASDEZAMORA("Lomas de Zamora"),
+	MERLO("Merlo"),
+	MORENO("Moreno"),
+	PILAR("Pilar"),
+	QUILMES("Quilmes"),
+	SANFERNANDO("San Fernando"),
+	SANISIDRO("San Isidro"),
+	SANMIGUEL("San Miguel"),
+	TIGRE("Tigre");
+	
+	private final String localityName;
+	
+	Locality(String name){
+		this.localityName = name;
+	}
+	
+	public String getLocalityName(){
+		return this.localityName;
+	}
 }
