13ed3c04c8fe168fdda15ac52c7eda7d514bdd6f
rozaracho <zaracho.rosali@gmail.com>
2017-08-23 16:14:05 -0300

    Un proveedor puede agregar un servicio

--- grupoc/src/main/java/model/Provider.java
+++ grupoc/src/main/java/model/Provider.java
@@ -1,10 +1,17 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import exception.InvalidServiceException;
+
 public class Provider {
-	private ServiceManager serviceManager;
+	private List<Service> servicesOffered;
 	
-	public void addNewService(){
-		Service newService= new Service();
-		this.serviceManager.addService(newService);
+	public Provider(){
+		this.servicesOffered= new ArrayList<Service>();
+	}
+	public void addNewService(Service newService) throws InvalidServiceException{
+			this.servicesOffered.add(newService);
 	}
 }
