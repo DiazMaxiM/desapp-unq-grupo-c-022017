0fa3ac4dd651647430855f15d51f05529bb89e08
Ingrid Calderón <ingridgcalderon@gmail.com>
2017-08-31 22:23:58 -0300

    Modifico tests de MapPosition por la incorporación de excepciones

--- grupoc/src/test/model/tests/MapPositionTest.java
+++ grupoc/src/test/model/tests/MapPositionTest.java
@@ -1,31 +1,40 @@
 package tests;
+
+import org.junit.Ignore;
 import org.junit.Test;
+
+import exception.InvalidLatitudeMapPositionException;
+import exception.InvalidLengthMapPositionException;
 import exception.InvalidMapPositionException;
 import model.MapPosition;
 
 public class MapPositionTest {
 
-	@Test(expected=InvalidMapPositionException.class)
-	public void testShouldFailWhenICreateAnInvalidMapPosition() throws InvalidMapPositionException {
-		new MapPosition(null,null);
+	@Test(expected = InvalidMapPositionException.class)
+	@Ignore
+	public void testShouldFailWhenICreateAnInvalidMapPosition()
+			throws InvalidMapPositionException, InvalidLengthMapPositionException, InvalidLatitudeMapPositionException {
+		new MapPosition(null, null);
+
 	}
-	
-	@Test(expected=InvalidMapPositionException.class)
-	public void testShouldFailWhenICreateAMapPositionWithLatitudeInvalid() throws InvalidMapPositionException {
-		new MapPosition(null,new Double(12));
+
+	@Test(expected = InvalidLatitudeMapPositionException.class)
+	public void testShouldFailWhenICreateAMapPositionWithLatitudeInvalid()
+			throws InvalidMapPositionException, InvalidLengthMapPositionException, InvalidLatitudeMapPositionException {
+		new MapPosition(null, new Double(12));
 	}
-	
-	@Test(expected=InvalidMapPositionException.class)
-	public void testShouldFailWhenICreateAMapPositionWithLengthInvalid() throws InvalidMapPositionException {
-		new MapPosition(new Double(12),null);
+
+	@Test(expected = InvalidLengthMapPositionException.class)
+	public void testShouldFailWhenICreateAMapPositionWithLengthInvalid()
+			throws InvalidMapPositionException, InvalidLengthMapPositionException, InvalidLatitudeMapPositionException {
+		new MapPosition(new Double(12), null);
 	}
-	
+
 	@Test()
-	public void testShouldPassWhenICreateAnValidMapPosition() throws InvalidMapPositionException {
-		new MapPosition(new Double(12),new Double(3.3333));
-		
+	public void testShouldPassWhenICreateAnValidMapPosition()
+			throws InvalidMapPositionException, InvalidLengthMapPositionException, InvalidLatitudeMapPositionException {
+		new MapPosition(new Double(12), new Double(3.3333));
+
 	}
-	
-	
 
 }
