09d141091fe7f73be7cd0741e56d9802eda83c00
rozaracho <zaracho.rosali@gmail.com>
2017-09-03 17:55:25 -0300

    Se agrego un validador para MapPosition

--- grupoc/src/main/java/model/MapPosition.java
+++ grupoc/src/main/java/model/MapPosition.java
@@ -2,35 +2,18 @@ package model;
 
 import exception.InvalidLatitudeMapPositionException;
 import exception.InvalidLengthMapPositionException;
+import validation.MapPositionValidation;
 
 public class MapPosition {
 	private Double latitude;
 	private Double length;
+	private MapPositionValidation validator= new MapPositionValidation();
 
 	public MapPosition(Double latitude, Double length)
 			throws InvalidLengthMapPositionException, InvalidLatitudeMapPositionException {
-		if (isAValidMapPosition(latitude, length)) {
+		if (validator.isAValidMapPosition(latitude, length)) {
 			createMapPosition(latitude, length);
 		}
-	}
-
-	private boolean isAValidMapPosition(Double latitude, Double lenght)
-			throws InvalidLengthMapPositionException, InvalidLatitudeMapPositionException {
-		return isValidLatitude(latitude) && isValidLength(lenght);
-	}
-
-	private boolean isValidLength(Double length) throws InvalidLengthMapPositionException {
-		if (length == null) {
-			throw new InvalidLengthMapPositionException("Debe ingresar una longitud válida");
-		}
-		return length != null;
-	}
-
-	private boolean isValidLatitude(Double latitude) throws InvalidLatitudeMapPositionException {
-		if (latitude == null) {
-			throw new InvalidLatitudeMapPositionException("Debe ingresar una latitud válida");
-		}
-		return latitude != null;
 	}
 
 	private void createMapPosition(Double latitude, Double length) {
---
+++ grupoc/src/main/java/validation/MapPositionValidation.java
@@ -0,0 +1,26 @@
+package validation;
+
+import exception.InvalidLatitudeMapPositionException;
+import exception.InvalidLengthMapPositionException;
+
+public class MapPositionValidation extends Validation {
+	public boolean isAValidMapPosition(Double latitude, Double lenght)
+			throws InvalidLengthMapPositionException, InvalidLatitudeMapPositionException {
+		return isValidLatitude(latitude) && isValidLength(lenght);
+	}
+
+	private boolean isValidLength(Double length) throws InvalidLengthMapPositionException {
+		if (length == null) {
+			throw new InvalidLengthMapPositionException("Debe ingresar una longitud válida");
+		}
+		return length != null;
+	}
+
+	private boolean isValidLatitude(Double latitude) throws InvalidLatitudeMapPositionException {
+		if (latitude == null) {
+			throw new InvalidLatitudeMapPositionException("Debe ingresar una latitud válida");
+		}
+		return latitude != null;
+	}
+
+}
