a0f6c8d43bb291493b13a09ac2cb6dcc6e74fbe2
Ingrid Calderón <ingridgcalderon@gmail.com>
2017-09-05 19:49:20 -0300

    Agrego la clase que testea el ordenamiento de menús

---
+++ grupoc/src/test/model/tests/SortingMenuByPriceTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class SortingMenuByPriceTest {
+
+	@Test
+	@Ignore
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
