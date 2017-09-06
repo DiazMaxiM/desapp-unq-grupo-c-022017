3f0c14def0bbccb6a91cfa68dd3b3ce3f2b4dcb0
Ingrid Calderón <ingridgcalderon@gmail.com>
2017-09-06 16:27:22 -0300

    Falta implementación del ordenamiento por puntuación

--- grupoc/src/main/java/sortingMenus/SortingMenuByScore.java
+++ grupoc/src/main/java/sortingMenus/SortingMenuByScore.java
@@ -1,15 +1,24 @@
 package sortingMenus;
 
+import java.util.ArrayList;
+
 import model.Menu;
 import model.MenuManager;
 
 public class SortingMenuByScore extends SortingMenuCriteria {
+
 	public SortingMenuByScore(MenuManager menuManager) {
 		super.setMenuManager(menuManager);
 	}
 
 	@Override
-	public Object getPropertyToOrder(Menu menu) {
+	public ArrayList<Menu> menusToOrder() {
+		return null;// this.getMenuManager().getScoringManager().getMenuWithAverage();
+	}
+
+	@Override
+	public ArrayList<Menu> orderMenu() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 }
