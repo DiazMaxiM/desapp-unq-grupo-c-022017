9f711dbc399729fe44851f6dc79e94c919ce7990
Ingrid Calderón <ingridgcalderon@gmail.com>
2017-09-06 16:10:12 -0300

    Implemento el ordenamiento de menús por precio

--- grupoc/src/main/java/sortingMenus/SortingMenuByPrice.java
+++ grupoc/src/main/java/sortingMenus/SortingMenuByPrice.java
@@ -1,5 +1,9 @@
 package sortingMenus;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 import model.Menu;
 import model.MenuManager;
 
@@ -10,8 +14,19 @@ public class SortingMenuByPrice extends SortingMenuCriteria {
 	}
 
 	@Override
-	public Object getPropertyToOrder(Menu menu) {
-		return menu.getMenuPrice();
+	public ArrayList<Menu> menusToOrder() {
+		return this.getMenuManager().getAllMenusOffered();
 	}
 
+	@Override
+	public ArrayList<Menu> orderMenu() {
+		ArrayList<Menu> allMenus = this.menusToOrder();
+		Comparator<Menu> comparator = new Comparator<Menu>() {
+			public int compare(Menu menuCurrent, Menu menuToCompare) {
+				return menuCurrent.getMenuPrice().getValue() < menuToCompare.getMenuPrice().getValue() ? -1 : 1;
+			}
+		};
+		Collections.sort(allMenus, comparator);
+		return allMenus;
+	}
 }
