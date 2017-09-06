b84280af054e63939e344b9107fb37fb51fd4dbc
Ingrid Calderón <ingridgcalderon@gmail.com>
2017-09-06 16:17:31 -0300

    Testeo el ordenamiento de menús por precio

--- grupoc/src/test/model/tests/SortingMenuByPriceTest.java
+++ grupoc/src/test/model/tests/SortingMenuByPriceTest.java
@@ -1,16 +1,85 @@
 package tests;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.Ignore;
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import builders.MenuBuilder;
+import exception.InvalidAddressException;
+import exception.InvalidAreaCodeException;
+import exception.InvalidAverageDeliveryTimeOfMenuException;
+import exception.InvalidCountryCodeException;
+import exception.InvalidLocalNumberException;
+import exception.InvalidLocalityAddressException;
+import exception.InvalidNumberStreetException;
+import exception.InvalidServiceException;
+import exception.InvalidStreetAddressException;
+import exception.InvalidTelephoneNumberException;
+import menuExceptions.InvalidDeliveryPriceException;
+import menuExceptions.InvalidEndDateOfferMenuException;
+import menuExceptions.InvalidFirstMinimumNumberOfMenusToBuyException;
+import menuExceptions.InvalidMaximumNumberOfMunusSalesPerDay;
+import menuExceptions.InvalidMenuCategoryException;
+import menuExceptions.InvalidMenuDeliveryPriceException;
+import menuExceptions.InvalidMenuDescriptionException;
+import menuExceptions.InvalidMenuNameException;
+import menuExceptions.InvalidMinimumNumberOfMenusToBuyException;
+import menuExceptions.InvalidMinimumPriceOfMenusToBuyException;
+import menuExceptions.InvalidStartDateOfferMenuException;
+import model.Menu;
+import model.MenuManager;
+import serviceException.InvalidServiceDescriptionException;
+import serviceException.InvalidServiceEmailException;
+import serviceException.InvalidServiceLogoException;
+import serviceException.InvalidServiceNameException;
+import serviceException.InvalidServiceWorkingHoursException;
+import sortingMenus.SortingMenuByPrice;
 
 public class SortingMenuByPriceTest {
 
 	@Test
-	@Ignore
-	public void test() {
-		fail("Not yet implemented");
+	public void testWhenIOrderAListOfMenusAndItIsEmptyItReturnsAnEmptyList() {
+		MenuManager menuManager = new MenuManager();
+		SortingMenuByPrice sortingMenuByPrice = new SortingMenuByPrice(menuManager);
+
+		assertEquals(0, menuManager.orderMenus(sortingMenuByPrice).size());
+	}
+
+	@Test
+	public void testWhenIOrderAListOfMenusWithMenusWithDifferentPricesReturnsAListSortedByPriceFromLowestToHighest()
+			throws InvalidServiceException, InvalidAddressException, InvalidNumberStreetException,
+			InvalidStreetAddressException, InvalidLocalityAddressException, InvalidLocalNumberException,
+			InvalidAreaCodeException, InvalidCountryCodeException, InvalidMenuNameException,
+			InvalidMenuDescriptionException, InvalidMenuCategoryException, InvalidStartDateOfferMenuException,
+			InvalidMenuDeliveryPriceException, InvalidFirstMinimumNumberOfMenusToBuyException,
+			InvalidMinimumNumberOfMenusToBuyException, InvalidMinimumPriceOfMenusToBuyException,
+			InvalidMaximumNumberOfMunusSalesPerDay, InvalidServiceNameException, InvalidServiceLogoException,
+			InvalidServiceDescriptionException, InvalidServiceEmailException, InvalidServiceWorkingHoursException,
+			InvalidDeliveryPriceException, InvalidEndDateOfferMenuException, InvalidAverageDeliveryTimeOfMenuException,
+			InvalidTelephoneNumberException {
+		MenuManager menuManager = new MenuManager();
+		Menu menu = new MenuBuilder().withMenuPrice(120).build();
+		Menu menu2 = new MenuBuilder().withMenuPrice(50).build();
+		Menu menu3 = new MenuBuilder().withMenuPrice(1).build();
+		Menu menu4 = new MenuBuilder().withMenuPrice(30).build();
+
+		menuManager.addMenuToMenusOffered(menu);
+		menuManager.addMenuToMenusOffered(menu2);
+		menuManager.addMenuToMenusOffered(menu3);
+		menuManager.addMenuToMenusOffered(menu4);
+
+		SortingMenuByPrice sortingMenuByPrice = new SortingMenuByPrice(menuManager);
+		ArrayList<Menu> menusOrdered = menuManager.orderMenus(sortingMenuByPrice);
+
+		assertEquals(4, menusOrdered.size());
+		assertTrue(menusOrdered.get(0).equals(menu3));
+		assertTrue(menusOrdered.get(1).equals(menu4));
+		assertTrue(menusOrdered.get(2).equals(menu2));
+		assertTrue(menusOrdered.get(3).equals(menu));
+
 	}
 
 }
