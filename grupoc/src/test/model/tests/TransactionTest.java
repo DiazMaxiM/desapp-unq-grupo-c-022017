14fc3f858eee7ae3c0fa22f6559dd181e0901f88
Maximiliano Diaz <diazmaxi@gmail.com>
2017-09-07 14:13:04 -0300

    correcion de nombre de clase tipo estado transaccion

--- grupoc/src/test/model/tests/TransactionTest.java
+++ grupoc/src/test/model/tests/TransactionTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import junit.framework.Assert;
 import model.Transaction;
+import model.TypeStatusTransaction;
 import model.TypeTransaction;
 
 public class TransactionTest {
@@ -12,11 +13,33 @@ public class TransactionTest {
 	public void testIfBuildANewTransactionAndIsCreditThenTheValueIsPositive() {
 		Transaction transaction = new Transaction(TypeTransaction.CREDIT, new Double(5.0));
 		Assert.assertEquals(transaction.getValueForSum(), new Double(5.0));
+		Assert.assertEquals(transaction.getTypeTransaction(), TypeTransaction.CREDIT);
+		Assert.assertEquals(transaction.getTypeStatusTransaction(), TypeStatusTransaction.PENDING);
+		Assert.assertTrue(transaction.isNotRejected());
+
 	}
 
 	@Test
 	public void testIfBuildANewTransactionAndIsDebitThenTheValueIsNegative() {
 		Transaction transaction = new Transaction(TypeTransaction.DEBIT, new Double(5.0));
+		Assert.assertEquals(transaction.getTypeTransaction(), TypeTransaction.DEBIT);
+
 		Assert.assertEquals(transaction.getValueForSum(), new Double(-5.0));
+	}
+
+	@Test
+	public void testIfBuildANewTransactionAndIsFinishThenTheTypeTransactionIsFINISH() {
+		Transaction transaction = new Transaction(TypeTransaction.DEBIT, new Double(5.0));
+		transaction.setFinish();
+		Assert.assertEquals(transaction.getTypeStatusTransaction(), TypeStatusTransaction.FINISH);
+	}
+
+	@Test
+	public void testIfBuildANewTransactionAndIsRejectedThenTheTypeTransactionIsRejected() {
+		Transaction transaction = new Transaction(TypeTransaction.DEBIT, new Double(5.0));
+		transaction.setRejected();
+		Assert.assertEquals(transaction.getTypeStatusTransaction(), TypeStatusTransaction.REJECTED);
+		Assert.assertFalse(transaction.isNotRejected());
+
 	}
 }
