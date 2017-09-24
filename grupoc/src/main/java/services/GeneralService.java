4a9454165588a23029b8e90989636d34a0db73cf
rozaracho <zaracho.rosali@gmail.com>
2017-09-24 10:27:36 -0300

    Arreglo mapeo

--- grupoc/src/main/java/services/GeneralService.java
+++ grupoc/src/main/java/services/GeneralService.java
@@ -3,15 +3,13 @@ package services;
 public class GeneralService {
 
 	public UserService userService;
-	
 
 	public UserService getUserService() {
 		return userService;
 	}
 
-
-	public void seUserService(final UserService userService) {
+	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
-
+	
 }
