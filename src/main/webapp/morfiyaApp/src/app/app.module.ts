fbdb759cbcc784b7e29f8fe623e4497ce8d083eb
Maximiliano Diaz <diazmaxi@gmail.com>
2017-10-14 09:22:29 -0300

    correccion app modules

--- src/main/webapp/morfiyaApp/src/app/app.module.ts
+++ src/main/webapp/morfiyaApp/src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { Http, HttpModule } from '@angular/http';
 
-export function createTranslateLoader(http: Http) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,14 +14,8 @@ export function createTranslateLoader(http: Http) {
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [Http]
-      }})
-      ],
+    HttpModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
