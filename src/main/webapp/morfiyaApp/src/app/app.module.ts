790324c7a264972fba849a1d1fed2935aac6fda7
Maximiliano Diaz <diazmaxi@gmail.com>
2017-10-14 09:31:41 -0300

    i18n funcionando

--- src/main/webapp/morfiyaApp/src/app/app.module.ts
+++ src/main/webapp/morfiyaApp/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
-
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,8 +17,14 @@ import { HomeComponent } from './home.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
-  ],
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }})
+      ],
   providers: [],
   bootstrap: [AppComponent]
 })
