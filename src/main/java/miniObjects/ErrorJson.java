487b604c21e83738b9367a98d2b72a0ed2286908
Maximiliano Diaz <diazmaxi@gmail.com>
2017-10-17 17:21:15 -0300

    creacion de objetos para rest (miniObjects)

---
+++ src/main/java/miniObjects/ErrorJson.java
@@ -0,0 +1,9 @@
+package miniObjects;
+
+public class ErrorJson {
+	public Integer code;
+
+	public ErrorJson(Integer code) {
+		this.code = code;
+	}
+}
---
+++ src/main/java/miniObjects/UserJson.java
@@ -0,0 +1,12 @@
+package miniObjects;
+
+public class UserJson {
+	public Integer id;
+	public String name;
+
+	public UserJson(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+}
