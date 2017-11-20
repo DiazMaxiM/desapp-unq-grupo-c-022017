328ca77ec43752db28d303f08749bbb6cfee8eca
rozaracho <zaracho.rosali@gmail.com>
2017-11-20 08:57:20 -0300

    se agregó el valor de la moneda actual

--- src/main/webapp/morfiyaApp/src/app/components/menus/menus.component.ts
+++ src/main/webapp/morfiyaApp/src/app/components/menus/menus.component.ts
@@ -5,10 +5,14 @@ import {MessageService} from './../../services/messageServices/message.service';
 import {Router} from '@angular/router';
 import {TranslateService} from '@ngx-translate/core';
 import {User} from './../../model/user';
+import { CurrencyPipe } from '@angular/common';
+import { CurrencyFormat } from './../../pipes/currencyFormat.pipe';
 import { UtilsService} from './../../services/utilsServices/utils.service';
 import { MenusService} from './../../services/menusServices/menus.service';
+import { LanguageService} from './../../services/languageService/languageService.service';
 import { ListMenusService} from './../../services/listMenusService/listMenus.service';
 declare var $:any;
+declare var currency: string;
 @Component({
   selector: 'menus',
   templateUrl: './menus.component.html',
@@ -18,13 +22,15 @@ declare var $:any;
 export class MenusComponent implements OnInit {
   p: number = 1;
   menus;
-  constructor(public listMenuService: ListMenusService,private router:Router,private menusService: MenusService,private translate: TranslateService,private utilsServices: UtilsService){
+  currency:String;
+  constructor(public languague:LanguageService ,public listMenuService: ListMenusService,private router:Router,private menusService: MenusService,private translate: TranslateService,private utilsServices: UtilsService){
   }
 
   
 
   ngOnInit(){
     this.listMenuService.currentMessage.subscribe(data=>this.menus=data);
+    this.languague.currentMessage.subscribe(message =>this.currency=message);
   }
 
   viewMenu(){
