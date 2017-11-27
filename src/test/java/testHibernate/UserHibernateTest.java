f60927b64ebe902115411eeb81f555fc999c3f49
rozaracho <zaracho.rosali@gmail.com>
2017-11-27 06:41:52 -0300

    test arquitectura guardar un usuario

--- src/test/java/testHibernate/UserHibernateTest.java
+++ src/test/java/testHibernate/UserHibernateTest.java
@@ -2,15 +2,30 @@ package testHibernate;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import builders.ClientBuilder;
+import exception.InvalidAddressException;
+import exception.InvalidAreaCodeException;
+import exception.InvalidCountryCodeException;
+import exception.InvalidLatitudeMapPositionException;
+import exception.InvalidLengthMapPositionException;
+import exception.InvalidLocalNumberException;
+import exception.InvalidLocalityAddressException;
+import exception.InvalidMapPositionException;
+import exception.InvalidNumberStreetException;
+import exception.InvalidStreetAddressException;
+import exception.InvalidTelephoneNumberException;
 import model.User;
 import services.UserService;
+import userExceptions.InvalidCuitException;
+import userExceptions.InvalidEmailAddressException;
+import userExceptions.InvalidFirstNameException;
+import userExceptions.InvalidLastNameException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "/META-INF/spring-persistence-context.xml", "/META-INF/spring-services-context.xml" })
@@ -18,12 +33,13 @@ public class UserHibernateTest {
 
 	@Autowired
 	private UserService userService;
-
-	@Ignore
+	
 	@Test
-	public void testSaveUser() {
-		userService.save(new User());
-		assertEquals(1, userService.retriveAll().size());
+	public void testSaveUser() throws InvalidAddressException, InvalidLocalNumberException, InvalidAreaCodeException, InvalidCountryCodeException, InvalidNumberStreetException, InvalidStreetAddressException, InvalidLocalityAddressException, InvalidLengthMapPositionException, InvalidLatitudeMapPositionException, InvalidTelephoneNumberException, InvalidMapPositionException, InvalidCuitException, InvalidFirstNameException, InvalidLastNameException, InvalidEmailAddressException {
+		User clientBuilder= new ClientBuilder().build();
+		userService.save(clientBuilder);
+		
+		assertEquals(userService.getRepository().findById(1), clientBuilder);
 	}
 
 }
